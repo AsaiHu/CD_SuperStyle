212ca7e7b253ec631382ad234161ce1183fba00e
ASAI <AsaiHu@163.com>
2019-01-27 17:30:46 +0800

    dad

---
+++ WebApp/html/assets/js/common.js
@@ -0,0 +1,22 @@
+﻿var contactUrl = "../Api/Web/GetContact";
+
+$(function () {
+    $("#head_container").load("./hb.html #head");
+    $("#footer_container").load("./hb.html #footer");
+    bindFooterContact();
+})
+
+function bindFooterContact() {
+    $.ajax({
+        type: 'POST',
+        data: null,
+        dataType: 'json',
+        url: contactUrl,
+        success: function (json) {
+            var data = json.data;
+            document.getElementsByClassName("footer_telephone")[0].innerText = data.Telephone;
+            document.getElementsByClassName("footer_mobilephone")[0].innerText = data.MobilePhone;
+            document.getElementsByClassName("footer_fax")[0].innerText = data.Fax;
+        }
+    })
+}
